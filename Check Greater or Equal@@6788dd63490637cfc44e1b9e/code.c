6ed6c8a1ff79f83c7ef9650fd2c73c466337c592
Muskan <muskanmittal249@gmail.com>
2025-01-25 19:29:11 +0530

    Solution to Check Greater or Equal

---
+++ Check Greater or Equal@@6788dd63490637cfc44e1b9e/code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int number_1,number_2;
+    scanf("%d %d",number_1,number_2);
+    if (number_1>=number_2){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+}
