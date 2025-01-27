e7d81647e5a179448a41af62a3b3f6f69ac6b404
Muskan <muskanmittal249@gmail.com>
2025-01-27 12:21:34 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -3,11 +3,11 @@ int main(){
     char grade;
     scanf("%c",&grade);
     switch (grade){
-        case 'A':printf("Excellent");
-        case 'B':printf("Good");
-        case 'C':printf("Average");
-        case 'D':printf("Below Average");
-        case 'F':printf("Fail");
+        case 'A':printf("Excellent");break;
+        case 'B':printf("Good");break;
+        case 'C':printf("Average");break;
+        case 'D':printf("Below Average");break;
+        case 'F':printf("Fail");break;
     }
     return 0;
 }
