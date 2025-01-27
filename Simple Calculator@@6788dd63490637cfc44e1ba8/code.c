61d138aa932752109554ed4f0df64e0d0b7aaaa2
Muskan <muskanmittal249@gmail.com>
2025-01-27 20:01:31 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int number1,number2;
+    char ch;
+    int sum=(number1+number2);
+    int sub=(number1-number2);
+    int multiply=(number1*number2);
+    int div=(number1/number2);
+    switch (ch){
+         case '+':printf("%d",sum);break;
+         case '-':printf("%d",sub);break;
+         case '*':printf("%d",multiply);break;
+         case '/':printf("%d",div);break;
+    }
+    return 0;
+}
