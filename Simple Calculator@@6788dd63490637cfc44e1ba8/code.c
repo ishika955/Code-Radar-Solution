9c0ebd9f445427aafee5bfc255d188ac82bcfbca
Muskan <muskanmittal249@gmail.com>
2025-01-27 20:11:30 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -6,13 +6,12 @@ int main() {
     int sum = n1 + n2;
     int sub = n1 - n2;
     int multiply = n1 * n2;
-    int div=n1/n2;
+    int div = n1 / n2;
     switch (ch){
          case '+':printf("%d\n",sum);break;
          case '-':printf("%d\n",sub);break;
          case '*':printf("%d\n",multiply);break;
          case '/':printf("%d\n",div);break;
-         default:printf("error");
-    }
-    return 0;
+         default:printf("error");}
+         return 0;
 }
