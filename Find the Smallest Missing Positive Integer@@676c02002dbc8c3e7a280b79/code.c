2d38b4a6ae084fd57469b26cdd04de4ff7ee2332
Muskan . <muskanmittal249@gmail.com>
2025-02-13 16:57:18 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -3,7 +3,7 @@ int main(){
     int N;
     scanf("%d",&N);
     for(int i=0;i<N;i++){
-        scanf("%d ",&arr[N]);
+        scanf("%d ",&arr[i]);
     }
     for(int i=0;i<N;i++){
        even=arr[i]%2==0;
